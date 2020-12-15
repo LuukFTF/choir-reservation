b8b8a1d9849d1d1d44e8aadb9aa82271e9c23e22
LuukFTF <lucasvdvegt@gmail.com>
2020-12-15 23:35:40 +0100

    initial commit

---
+++ public/colorpallette/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,
+    initial-schale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css">
+    <title>Color Test</title>
+</head>
+<body>
+    <div class="container">
+        <div class="title">RC Dark Theme</div>
+    </div>
+    <div class="container flex">
+        <div class="flexitem" id="--primary-color"><h2>Primary</h2><p>500</p></div>
+        <div class="flexitem" id="--primary-color2"><h2>Primary 2</h2><p>200</p></div>
+        <div class="flexitem" id="--bg-color"><h2>Background</h2><p>900</p></div>
+        <div class="flexitem" id="--surface-color1"><h2>Surface 1</h2><p>600</p></div>
+        <div class="flexitem" id="--surface-color2"><h2>Surface Main</h2><p>700</p></div>
+        <div class="flexitem" id="--surface-color3"><h2>Surface 3</h2><p>800</p></div>
+        <div class="flexitem" id="--error-color"><h2>Error</h2><p>200</p></div>
+    </div>
+</body>
+</html>
+
