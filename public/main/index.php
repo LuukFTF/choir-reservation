8ca7697bf48e232f13d8ef30662450c898babf0e
LuukFTF <lucasvdvegt@gmail.com>
2020-12-17 22:53:47 +0100

    first dev

--- public/main/index.php
+++ public/main/index.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width,
     initial-schale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/utilities.css">
     <title>1. Homepage</title>
 </head>
 <body>
@@ -17,11 +18,12 @@
     </div>
 
     <section class="bottomnavbar">
-            <div class="container flex">
-                <div class="button">
-
-                </div>
-            </div>
+            <nav class="container flex nav">
+                <a class="btn bottom-btn">Home</a>
+                <a class="btn bottom-btn">Events</a>
+                <a class="btn bottom-btn">Inbox</a>
+                <a class="btn bottom-btn">Settings</a>
+            </nav>
         </div>
     </section>
 </body>
