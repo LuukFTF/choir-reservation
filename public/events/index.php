514c8318ecb6c4736ec77f91fc8f328c2f79cc99
LuukFTF <lucasvdvegt@gmail.com>
2021-01-11 23:38:25 +0100

    calendar + topbar

--- public/events/index.php
+++ public/events/index.php
@@ -12,9 +12,11 @@
 <body>
 
     <div class="webcontainer">
-        <section class="topbar">
-            <div class="title"> Month </div> 
-            <a class="btn topbar-btn btn-options"></a>
+        <section class="topbar-container">
+            <div class="topbar container flex">
+                <div class="title flex-item"> January </div> 
+                <a class="btn topbar-btn btn-options flex-item"></a>
+            </div>
         </section>
 
         <section class="calendar-list callist-default">
@@ -25,18 +27,59 @@
                         <div class="dayofmonth">1</div>
                     </div>
                     <div class="callist-right item">
-                        <div class="title">Voorbereiding</div>
+                        <div class="title">Item 1</div>
                         <div class="time">19:00 - 23:15</div>
                     </div>
                 </div>
+
                 <div class="calllist-item flex-item container flex">
                     <div class="callist-left flex-item">
                         <div class="day">fri</div>
                         <div class="dayofmonth">8</div>
                     </div>
                     <div class="callist-right item">
-                        <div class="title">Show</div>
+                        <div class="title">Item 2</div>
                         <div class="time">13:00 - 18:00</div>
+                    </div>
+                </div>
+                <div class="calllist-item flex-item container flex">
+                    <div class="callist-left flex-item">
+                        <div class="day">tue</div>
+                        <div class="dayofmonth">12</div>
+                    </div>
+                    <div class="callist-right item">
+                        <div class="title">Item 3</div>
+                        <div class="time">13:00 - 18:00</div>
+                    </div>
+                </div>
+                <div class="calllist-item flex-item container flex">
+                    <div class="callist-left flex-item">
+                        <div class="day">wed</div>
+                        <div class="dayofmonth">13</div>
+                    </div>
+                    <div class="callist-right item">
+                        <div class="title">Item 4</div>
+                        <div class="time">13:00 - 18:00</div>
+                    </div>
+                </div>
+                <div class="calllist-item flex-item container flex">
+                    <div class="callist-left flex-item">
+                        <div class="day">sat</div>
+                        <div class="dayofmonth">16</div>
+                    </div>
+                    <div class="callist-right item">
+                        <div class="title">Item 5</div>
+                        <div class="time">19:00 - 23:15</div>
+                    </div>
+                </div>
+                <div class="calllist-item flex-item container flex">
+                    <div class="callist-left flex-item">
+                        <div class="day">sat</div>
+                        <div class="dayofmonth">16</div>
+                    </div>
+                    <div class="callist-right item">
+                        <div class="title">Item 5</div>
+                        <div class="time">19:00 - 23:15</div>
                     </div>
                 </div>
             </div>
@@ -48,13 +91,12 @@
     </div>
 
     <section class="bottomnavbar-container">
-            <nav class="bottomnavbar container flex nav">
-                <a href="../main" class="btn nav-btn">Home</a>
-                <a href="../events" class="btn nav-btn active">Events</a>
-                <a class="btn nav-btn">Inbox</a>
-                <a class="btn nav-btn">Settings</a>
-            </nav>
-        </div>
+        <nav class="bottomnavbar container flex nav">
+            <a href="../main" class="btn nav-btn">Home</a>
+            <a href="../events" class="btn nav-btn active">Events</a>
+            <a class="btn nav-btn">Inbox</a>
+            <a class="btn nav-btn">Settings</a>
+        </nav>
     </section>
 </body>
 </html>
